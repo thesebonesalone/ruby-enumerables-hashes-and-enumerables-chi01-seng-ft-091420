ed3f184df072dc7a7c76bc838b3ddf3cee644794
Willem A Duiker <duikhead2@gmail.com>
2020-08-22 16:05:02 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -11,9 +11,9 @@ def select_winner(passengers)
   winner = ""
   
   passengers.each do |key, value|
-    if key == "suite_a" %% value.start_with?("A")
+    if key == :suite_a %% value.start_with?("A")
       winner = value
     end
-    winner
   end
+  winner
 end
