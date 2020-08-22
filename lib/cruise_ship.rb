6915496752c1ee71a08d0a6cc838740d8ba6280d
Willem A Duiker <duikhead2@gmail.com>
2020-08-22 16:07:02 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,4 +8,12 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = ""
+  
+  passengers.each do |key, value|
+    if key == :suite_a && value[0].capitalize() == "A"
+      winner = value
+    end
+  end
+  winner
 end
